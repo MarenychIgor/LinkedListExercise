2ab70888de2fd4c52ef3ba9e891894775ff69f7f
Igor Marenych <marenych.igor@gmail.com>
2019-06-07 01:38:26 +0300

    Add unit tests

---
+++ NodeExerciseTests/NodeExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using NodeExercise;
+using Xunit;
+
+namespace NodeExerciseTests
+{
+    public class NodeExtensionsTest
+    {
+        [Fact]
+        public void GetReversedRecursive_NotEmptyInput_ReturnsCorrectSequence()
+        {
+            var sut = new Node(2);
+
+            var actual = sut.GetReversedRecursive();
+
+            Assert.Equal(2, actual.Index);
+            Assert.NotNull(actual.Next);
+
+            Assert.Equal(1, actual.Next.Index);
+            Assert.Null(actual.Next.Next);
+        }
+
+        [Fact]
+        public void GetReversedIterative_NotEmptyInput_ReturnsCorrectSequence()
+        {
+            var sut = new Node(2);
+
+            var actual = sut.GetReversedIterative();
+
+            Assert.Equal(2, actual.Index);
+            Assert.NotNull(actual.Next);
+
+            Assert.Equal(1, actual.Next.Index);
+            Assert.Null(actual.Next.Next);
+        }
+    }
+}
